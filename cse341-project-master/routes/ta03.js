8c2e0d57224d33a41657650b978959180d5e19b2
polsongarrett <garrettpolson94@gmail.com>
2021-10-01 20:43:08 -0600

    Finished team03 activity

--- cse341-project-master/routes/ta03.js
+++ cse341-project-master/routes/ta03.js
@@ -1,13 +1,63 @@
-//TA03 PLACEHOLDER
 const express = require('express');
+const https = require('https');
+
 const router = express.Router();
+const fileName ='https://byui-cse.github.io/cse341-course/lesson03/items.json';
+
+let contents='';
+
+https.get(fileName,(res) => {
+  let body = "";
+
+  res.on("data", (chunk) => {
+    body += chunk;
+  });
+
+  res.on("end", () => {
+    try {
+      contents = JSON.parse(body);
+    } catch (error) {
+      console.error(error.message);
+    };
+  });
+}).on("error", (error) => {
+    console.error(error.message);
+});
 
 router.get('/', (req, res, next) => {
   res.render('pages/ta03', {
     title: 'Team Activity 03',
     path: '/ta03', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+    json: contents,
+    filterMesh: contents
+  });
+});
+
+router.get('/search', (req, res, next) => {
+  let filterJson = contents;
+  const search = req.query.keyword;
+
+  if(search !== undefined) {
+    filterJson = contents.filter((item)=>{
+      const mesh = search;
+
+      if(item.name.includes(mesh)||item.description.includes(mesh)) {
+        return true;
+      }
+
+      for(tag of item.tags) {
+        if(tag.includes(mesh)) {
+          return true;
+        } 
+      }
+    });
+  }
+
+  res.render('pages/ta03', {
+    title: 'Team Activity 03',
+    path: '/ta03', // For pug, EJS
+    json: contents,
+    filterMesh: filterJson
   });
 });
 
