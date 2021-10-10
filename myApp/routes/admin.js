1b822817edac32cadf5bbaffd8208dc68e8218b1
polsongarrett <garrettpolson94@gmail.com>
2021-10-09 18:03:43 -0600

    completed Prove04

--- myApp/routes/admin.js
+++ myApp/routes/admin.js
@@ -2,30 +2,23 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path')
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-const products = [];
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    docTitle: 'Add Product', 
-    path: '/admin/add-product',
-    activeAddProduct: true,
-    productCSS: true
-  });
-});
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-router.post('/add-product', (req, res, next) => {
-  products.push(
-    {
-      title: req.body.title, 
-      price: req.body.price, 
-      description: req.body.description
-    });
-  res.redirect('/');
-});
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
+
+module.exports = router;
